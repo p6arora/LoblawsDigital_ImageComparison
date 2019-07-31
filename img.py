c7273249bb24cd4d0b54e3b406911865c53e639e
Paarth Arora <paarth.arora21@gmail.com>
2019-07-31 05:01:05 -0400

    final formatting

--- img.py
+++ img.py
@@ -5,8 +5,11 @@ import logging
 
 logging.basicConfig(filename='result.log', level=logging.DEBUG, format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s' )
 
-#read from CSV file
+
 def read_data():
+    """
+    read from CSV file
+    """
     logging.debug("starting to read data")
     with open('input_test_data.csv') as input_data:
         logging.debug("opened data file")
@@ -19,33 +22,39 @@ def read_data():
         line_count += 1
         return [original_image, image_to_compare]
 
-# write column titles for result CSV file
 def write_header():
+    """
+    write column titles for result CSV file
+    """
     global line_count
     with open('result_data.csv', mode='w') as result:
         result_writer = csv.writer(result, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         result_writer.writerow(['image1', 'image2', 'similarity_ratio', 'time_elapsed'])
     logging.debug('wrote result header')
     line_count += 1
-
-# count total number of rows in CSV file - i.e how many comparisons need to be done
+ 
 def count_image_pairs():
+    """
+    count total number of rows in CSV file - i.e how many comparisons need to be done
+    """
     with open('input_test_data.csv') as input_data:
         logging.debug("counting number of rows in CSV file")
         csv_reader = csv.reader(input_data, delimiter=',')
         return sum(1 for row in csv_reader)
 
-
-# reading images
 def read_images(original_img: str, image_to_compare: str):
+    """
+    reading images
+    """
     original_image = cv2.imread(original_img)
     compared_image = cv2.imread(image_to_compare)
     logging.debug("Read images")
     return [original_image, compared_image]
-    
-
-# preliminary check if RGB values are same 
+     
 def check_identical(original_image, compared_image, start_time: float, image_names):
+    """
+    preliminary check if RGB values are same 
+    """
     # if images are the same shape and size - could be identical
     if original_image.shape == compared_image.shape:
         logging.debug("Images have the same size and channel")
@@ -62,18 +71,23 @@ def check_identical(original_image, compared_image, start_time: float, image_nam
             logging.debug("The images are NOT completely Equal")
             return False
 
-# if images are not identical:
-# use OpenCV's SIFT Algorithm to find key points and features between images
 def generate_keypoints(original_image: str, compared_image: str):
+    """
+     if images are not identical:
+     use OpenCV's SIFT Algorithm to find key points and features between images
+    """
     sift = cv2.xfeatures2d.SIFT_create()
     key_point_image1, descriptor_image1 = sift.detectAndCompute(original_image, None)
     key_point_image2, descriptor_image2 = sift.detectAndCompute(compared_image, None)
     logging.debug("Generated keypoints for both images")
     return [key_point_image1, descriptor_image1, key_point_image2, descriptor_image2]
 
-# FLANN - Fast Library for Approximate Neighbors
-# uses Euclidean distance between common key points to judge similarity  
+ 
 def compare_images(key_point_image1, descriptor_image1, key_point_image2, descriptor_image2):
+    """
+     FLANN - Fast Library for Approximate Neighbors
+     uses Euclidean distance between common key points to judge similarity 
+    """
     index_params = dict(algorithm=0, trees=5)
     search_params = dict()
     flann = cv2.FlannBasedMatcher(index_params, search_params)
@@ -81,9 +95,12 @@ def compare_images(key_point_image1, descriptor_image1, key_point_image2, descri
     logging.debug("found common points in images")
     return matches
     
-# SIFT algorithm has high recall, but low precision
-# so a threshold ratio needs to be set to filter inacurate results
+
 def filter_results(matches: enumerate):
+    """
+     SIFT algorithm has high recall, but low precision
+     so a threshold ratio needs to be set to filter inacurate results
+    """
     good_points = 0
     ratio = 0.6 # approximated through trial and error
     logging.debug("Matches:" + str(len(matches)))
@@ -92,23 +109,28 @@ def filter_results(matches: enumerate):
                     good_points += 1
     logging.debug("Good Matches: " + str(good_points))
     return [good_points, len(matches)]
-    
-
-# Generate Similarity Score
+     
 def generate_similarity_score(correct_matches: int, total_matches: int):
+    """
+    Generate Similarity Score
+    """
     return 1.0 - correct_matches/total_matches   # as defined in requirements, values close to 0 is most similar, so more correct matches mean similar pictures
 
-# Publish to CSV
 def publish_results(original_img: str, image_to_compare: str, similarity_ratio, start_time):
+    """
+    Publish to CSV
+    """
     logging.debug("writing to CSV file...")
     with open('result_data.csv', mode='a', newline='') as result:
         result_writer = csv.writer(result, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         result_writer.writerow([original_img, image_to_compare, str(similarity_ratio), str(round(time.time() - start_time, 2))] )
         logging.debug("Write successful")
 
-# Main Function
-# reading data from CSV
 def main():
+    """
+     Main Function
+     reading data from CSV
+    """
     logging.debug("starting program")
     global line_count
     line_count = 0
